c78d79edf850fae1b2c2634ef08700ad61f6ea4a
James <kenjisan231@users.noreply.github.com>
2017-04-12 02:06:55 -0400

    renamed format_datetime() to datetime_information() and moved it to journal-main.py.datetime_information() now returns a dictionary of current date, weekday, and time for greater flexibility in what information is saved once user completes entry

--- journal-main.py
+++ journal-main.py
@@ -1,20 +1,74 @@
 #! python3
 
-import sys, argparse
+"""Program for logging simple journal entries"""
+import time
+from journal_file_handling import (get_entry_count)
 
-"""Program for logging simple journal entries to .txt files via cmd line"""
-# def get_entry():
-#     print("Batch successful")
 
+def datetime_information():
+    """Returns dictionary of current date, time, and day of week based on easten
+    standard time
+    
+    return example:
+    {
+        "date": Jan-1-1970
+        "time": 04:00:00
+        "day" : Thursday
+    }
+
+    """
+
+    current_datetime = time.localtime()
+
+    months_dictionary = {1:  "Jan",
+                         2:  "Feb",
+                         3:  "Mar",
+                         4:  "Apr",
+                         5:  "May",
+                         6:  "Jun",
+                         7:  "Jul",
+                         8:  "Aug",
+                         9:  "Sep",
+                         10: "Oct",
+                         11: "Nov",
+                         12: "Dec"}
+    
+    days_dictionary =   {1 : "Monday",
+                         2 : "Tuesday",
+                         3 : "Wednesday",
+                         4 : "Thursday",
+                         5 : "Friday",
+                         6 : "Saturday",
+                         7 : "Sunday"}
+
+    #date information
+    month   = months_dictionary[current_datetime.tm_mon]
+    day     = current_datetime.tm_mday
+    year    = current_datetime.tm_year
+    weekday = days_dictionary[current_datetime.tm_wday]
+    
+    #time information
+    hour    = current_datetime.tm_hour
+    minute  = current_datetime.tm_min
+    second  = current_datetime.tm_sec
+
+    #formatted information
+    return {"date": "{}-{}-{}".format(month, day, year),
+            "time": "{}:{}:{}".format(hour, minute, second),
+            "day" :  weekday}
+
+
+def get_entry():
+
+    print("** Begin log for {} **\n".format(datetime_information()["date"]))
+    user_entry = input("Entry #{}: ".format(get_entry_count() + 1))
+
+    return user_entry
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("echo", help="echo the string entered")
-    # args = parser.parse_args()
-
-
-
-    # print(args.echo)
-
-    x = input("Log entry: ")
+    x = get_entry()
     print(x)
+
+
+
+   
--- journal_file_handling.py
+++ journal_file_handling.py
@@ -1,45 +1,37 @@
 #! python3
 
-"""Contains functions that construct a filename based on current date"""
-import time
+"""Functions for writing user input to files, and updating entry count data"""
 import json
 import os
 
-def format_datetime():
-    """Returns a string representative of the current date based on eastern
-    standard time.  
 
-    Returned string will have the following format: Jan-1-1970"""
+def get_entry_count(update_count=False):
+    """Either returns current journal entry count, or sends that count to
+    get_entry_county()
 
-    current_date = time.localtime()
+    Arguments:
+    update_count -- if True, sends count to update_entry_count() to be updated
 
-    months_dictionary = {1:  "Jan",
-                         2:  "Feb",
-                         3:  "Mar",
-                         4:  "Apr",
-                         5:  "May",
-                         6:  "Jun",
-                         7:  "Jul",
-                         8:  "Aug",
-                         9:  "Sep",
-                         10: "Oct",
-                         11: "Nov",
-                         12: "Dec"}
-    
-    month = months_dictionary[current_date.tm_mon]
-    day = current_date.tm_mday
-    year = current_date.tm_year
-
-    return "{}-{}-{}".format(month, day, year)
-    
-
-def get_entry_count():
-    """returns total count of journal entries from entry_count.json"""
+    """
 
     current_directory = os.getcwd()
-    filename = "{}\\json_data\\entry_count.json".format(current_directory)
+    filename = "{}\\json_data\\entry_count.json".format(current_directory)    
     with open(filename) as f_obj:
-        return json.load(f_obj)
+        entry_count = json.load(f_obj)
+        
+    if update_count:
+        update_entry_count(filename, entry_count)
+    else:
+        return entry_count
+
+def update_entry_count(filename, entry_count):
+    """Updates entry_count.json file"""
+
+    with open(filename, 'w') as f_obj:
+        json.dump(entry_count+1, f_obj)
+
+
+
 
 if __name__ == '__main__':
     #test case
