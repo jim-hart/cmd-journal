8a114815715f2c323ac016e9471dc906a776f5bb
James <jim-hart@users.noreply.github.com>
2017-04-21 01:40:56 -0400

    moved formatting of file and body to journal_main.py into class Entry

--- journal_main.py
+++ journal_main.py
@@ -2,8 +2,61 @@
 
 """Program for logging simple journal entries"""
 import sys
-import journal_file_handling
 import datetime_information
+from journal_file_handling import (Directory, JsonData)
+
+class Entry:
+    """Creates Entry object, which holds a formatted filename and text body"""
+    
+    def __init__(self, entry):
+        self.file_data = JsonData().data
+        self.entry     = entry
+        self.datetime  = datetime_information.get_datetime()
+        self.body      = format_body()
+        self.filename  = format_filename()
+        
+        
+    def format_body(self):
+        """Returns list containing header information, and the users text entry
+        
+        Arguments:
+        entry -- string containing user's journal entry
+            
+        .txt header format example:
+        
+        Date: Jan-1-1970
+        Day : Thursday    
+        Time: 04:00:00
+        Log#: 1
+                
+        Entry: <user's text here>       
+        
+        """  
+        
+        return  ["Date: {}\n".format(self.datetime["date"]),
+                 "Day : {}\n".format(self.datetime["day"]),
+                 "Time: {}\n".format(self.datetime["time"]),
+                 "Log#: {}\n\n".format(file_data.data["entry_count"]+1),
+                 "Entry:{}".format(self.entry)]
+     
+    
+    def format_filename(self):
+        """Returns filename formatted with datetime information, and entry number
+        
+        Example:
+        
+        Entry #1 -- Jan-1-1970
+        
+        """
+
+        return "{}\\Entry #{} -- {}.txt".format(
+                                        Directory.ENTRIES,
+                                        self.filedata["entry_count"]+1,
+                                        self.datetime["date"])
+
+
+def save_entry(entry, file_data):
+    
 
 
 def confirm_entry(selection):
@@ -11,15 +64,30 @@ def confirm_entry(selection):
     
     Arguments:
     selection  -- string value that dictates which function is passed control
+       "-qc" Exits the program; no file is written to or modified
         "-s" Returns True, resulting in main() passing control to save_entry()
         "-e" Returns False, resulting in main() allowing user to re-enter entry   
             
     """
-       
-    if selection == "-s":
+    
+    if selection == "-qc":
+        sys.exit("Entry Cancelled")   
+    elif selection == "-s":
         return True
     elif selection == "-e":
         return False
+    
+def get_entry():
+    user_initial = input("Entry #{}: ".format(file_data.data["entry_count"]+1))
+    user_final = "{} {}".format("", user_initial)
+    selection = input("-s: save entry, -e: edit entry, -qc: quit & cancel: ")
+    
+    #simple error checking process to verify journal entry creation
+    selection_list = ["-s", "-e", "-qc"]    
+    while selection not in selection_list:
+        print("Error: Invalid Selection")
+        selection = input("-s: save entry, -e: edit entry, -qc: quit & cancel: ")
+
 
 def main():
     """Gets user's journal input and passes it to confirm_entry() based on
@@ -30,26 +98,18 @@ def main():
     print("** Begin log for {} **\n".format(
                                 datetime_information.get_datetime()["date"]))
     
-    user_initial = input("Entry #{}: ".format(file_data.data["entry_count"]+1))
-    user_final = "{} {}".format("", user_initial)
-    selection = input("-s: save entry, -e: edit entry, -qc: quit & cancel: ")
     
-    #simple error checking process to verify journal entry creation
-    selection_list = ["-s", "-e", "-qc"]    
-    while selection not in selection_list:
-        print("Error: Invalid Selection")
-        selection = input("-s: save entry, -e: edit entry, -qc: quit & cancel: ")
         
-    if selection == "-qc":
-        sys.exit("Entry Cancelled")
+    while not confirm_entry(selection):
     
-    if confirm_entry(selection):
-        journal_file_handling.save_entry(user_final, file_data)
+    
+    journal_file_handling.save_entry(user_final, file_data)
+    
    
 
 if __name__ == '__main__':
-    main()
-
+    entry = Entry()
+    print(entry.file_data)
 
 
    
