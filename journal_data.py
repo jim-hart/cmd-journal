d965b4beb0ecc280029c0ab60922eed046651a89
James <jim-hart@users.noreply.github.com>
2017-04-28 01:52:42 -0400

    For Directory class: Added json_path() method to return path to json folder location.  Also added check_entry_path() to see if a folder path for entries exists within the programs json data file.  If one isn't, a default folder is created in the program's working directory

--- journal_data.py
+++ journal_data.py
@@ -3,36 +3,55 @@
 """Functions for writing user input to files, and updating entry_count data"""
 
 import json
-import os, sys
+import os
+import sys
 
 
 class Directory:
     """Holds directory information for files used in program"""
-    
-    #JSON    = 
+
     ENTRIES = 'Q:\\GDrive\\Books & Notes\\Entries'
-    
+
     @classmethod
     def json_path(cls):
-        return os.path.join(os.path.dirname(sys.argv[0]), 
-                          'json_data\\file_information.json')
+        """Returns path of folder holding json data for the program"""
+
+        return os.path.join(os.path.dirname(sys.argv[0]),
+                            'json_data\\file_information.json')
+
+    @classmethod
+    def check_entry_path(cls):
+        """Returns true if entry path folder exists, otherwise, default path is
+        set for user in working directory for program"""
+
+        if not os.path.exists(JsonData.get_data()['entry_path']):
+            default_path = os.path.join(os.path.dirname(sys.argv[0]), 'Entries')
+            print("Enter Folder Location not detected! Folder set to: {}"
+                  .format(default_path))
+            os.mkdir(default_path)
+
+        else:
+            return True
+
+    
+
 
 class JsonData:
-    """Class for managing file_information.json"""        
-    
+    """Class for managing file_information.json"""
+
     @classmethod
     def get_data(cls):
         """Returns dictionary containing keys:values in file_information.json"""
-                     
+
         with open(Directory.json_path()) as f_obj:
             return json.load(f_obj)
-            
+
     @classmethod
     def update_count(cls):
         """Iterates entry_count value by 1 and writes new value to
         file_information.json """
-        
-        file_data = cls.get_data()        
+
+        file_data = cls.get_data()
         with open(Directory.json_path(), 'w') as f_obj:
             file_data['entry_count'] += 1
             json.dump(file_data, f_obj, indent=4, sort_keys=True)
@@ -46,17 +65,18 @@ def save_entry(entry):
         entry -- Object containing formatted text header and filename
         
     """
-    
+
     JsonData().update_count()
 
     with open(os.path.join(Directory.ENTRIES, entry.filename), 'w') as f_obj:
         f_obj.writelines(entry.header)
         f_obj.writelines(entry.body)
-    
+
     print("Entry saved to {}".format(Directory.ENTRIES))
-    
-    
+
+
 if __name__ == '__main__':
     # test case(s)
-    #JsonData().update_count()
-    print(Directory.json_path())
+    # Directory.check_entry_path()
+    Directory.set_entry_path()
+    # print(Directory.json_path())
