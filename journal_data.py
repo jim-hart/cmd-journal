3d61722073b2160483659450da999496b81f393d
James <jim-hart@users.noreply.github.com>
2017-04-28 02:39:57 -0400

    json_data() under Directory class renamed to get_path(), which now accepts two arguments.  json_data=True will return path to json data, while entry_data=True will return path to entry files)

--- journal_data.py
+++ journal_data.py
@@ -5,7 +5,7 @@
 import json
 import os
 import sys
-
+import shutil
 
 class Directory:
     """Holds directory information for files used in program"""
@@ -13,16 +13,20 @@ class Directory:
     ENTRIES = 'Q:\\GDrive\\Books & Notes\\Entries'
 
     @classmethod
-    def json_path(cls):
+    def get_path(cls, json_data=False, entry_data=False):
         """Returns path of folder holding json data for the program"""
 
-        return os.path.join(os.path.dirname(sys.argv[0]),
-                            'json_data\\file_information.json')
+        if json_data:
+            return os.path.join(os.path.dirname(sys.argv[0]),
+                               'json_data\\file_information.json')
+        elif entry_data:
+            return JsonData.get_data()['entry_path']
 
     @classmethod
     def check_entry_path(cls):
-        """Returns true if entry path folder exists, otherwise, default path is
-        set for user in working directory for program"""
+        """Returns true if entry path folder exists in file_information.json,
+        otherwise, default path is set for user in working directory for
+        program"""
 
         if not os.path.exists(JsonData.get_data()['entry_path']):
             default_path = os.path.join(os.path.dirname(sys.argv[0]), 'Entries')
@@ -33,8 +37,20 @@ class Directory:
         else:
             return True
 
-    
-
+    @classmethod
+    def set_entry_path(cls, path):
+        """Changes path to entry folder and copies all current entries to that
+        new folder
+        
+        Args:
+            path (str): string that contains path to new folder location
+        
+        """  
+        
+        # current_path = cls.get_path()
+        # shutil.copytree(
+        # JsonData.update_data(entry_path=True, new_path=path)
+        
 
 class JsonData:
     """Class for managing file_information.json"""
@@ -43,30 +59,40 @@ class JsonData:
     def get_data(cls):
         """Returns dictionary containing keys:values in file_information.json"""
 
-        with open(Directory.json_path()) as f_obj:
+        with open(Directory.get_path()) as f_obj:
             return json.load(f_obj)
 
     @classmethod
-    def update_count(cls):
-        """Iterates entry_count value by 1 and writes new value to
-        file_information.json """
+    def update_data(cls, entry_count=False, entry_path=False, new_path=""):
+        """Performs action based on passed entry_count or entry_path arguments 
+
+        Args:            
+            entry_count (bool): if True, entry_count is iterated by 1
+            entry_path (bool):  if True, entry_path will be set to new_path parameter
+            new_path (str):     contains string holding new path to entry folder
+        
+        """
 
         file_data = cls.get_data()
-        with open(Directory.json_path(), 'w') as f_obj:
-            file_data['entry_count'] += 1
+        with open(Directory.get_path(), 'w') as f_obj:
+            if entry_count:
+                file_data['entry_count'] += 1 
+            elif entry_path:
+                file_data['entry_path'] = new_path
+                
             json.dump(file_data, f_obj, indent=4, sort_keys=True)
-
+            
 
 def save_entry(entry):
     """Writes user's journal entry and datetime information to a .txt file and
-    calls update_count() from JsonData() to iterate entry count by 1
+    calls update_data() from JsonData() to iterate entry count by 1
     
     Arguments:
-        entry -- Object containing formatted text header and filename
+        :param entry: Object containing formatted text header and filename
         
     """
 
-    JsonData().update_count()
+    JsonData().update_data(entry_count=True)
 
     with open(os.path.join(Directory.ENTRIES, entry.filename), 'w') as f_obj:
         f_obj.writelines(entry.header)
@@ -78,5 +104,5 @@ def save_entry(entry):
 if __name__ == '__main__':
     # test case(s)
     # Directory.check_entry_path()
-    Directory.set_entry_path()
-    # print(Directory.json_path())
+    Directory.check_entry_path()
+    # print(Directory.get_path())
